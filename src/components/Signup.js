da065b398dcdf281fc79f58d38984de2bea624fb
olga <oluciams84@gmail.com>
2022-06-15 11:02:34 -0500

    add logic to capture the value of each input of the singup component

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,25 +1,91 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './signup.css'
 
 
 export const Signup = ()=>{
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmationPassword, setConfirmationPassword] = useState('');
+
+  const handleName = (e) =>{
+    setName(e.target.value)
+    console.log(name)
+  }
+
+  const handleEmail = (e)=>{
+    setEmail(e.target.value)
+  }
+
+  const handlePassword = (e)=>{
+    setPassword(e.target.value)
+  }
+
+  const handleConfirmationPassword = (e)=>{
+    setConfirmationPassword(e.target.value)
+  }
+
+  const handleOnSubmit = (e)=>{
+    e.preventDefault(); 
+    if(name && email && password && confirmationPassword) {
+      saveFormData(name, email, password, confirmationPassword)
+      setName('');
+      setEmail('');
+      setPassword('');
+      setConfirmationPassword('');      
+    } 
+  }
+
+  const saveFormData = ()=>{
+    const formSingup = {name, email, password, confirmationPassword}
+    console.log(formSingup)
+  }
+
   return(   
     <section className='register'> 
       <h2 className='register__title'>Sign Up</h2>
-      <form className='resgister__form' method='POS' action=''>
+      <form onSubmit={handleOnSubmit} className='resgister__form' method='POS' action=''>
         <div>        
-          <input className='register__input' type='tex' name='name' placeholder='Full Name'/>
+          <input 
+            className='register__input'
+            type='tex'
+            value={name}
+            onChange={handleName}
+            name='name'
+            placeholder='Full Name'/>
         </div>
         <div>        
-          <input className='register__input' type='email' name='email' placeholder='Email'/>    
+          <input
+            className='register__input'
+            type='email'
+            value={email}
+            onChange={handleEmail}
+            name='email'
+            placeholder='Email'/>    
         </div>
         <div>        
-          <input className='register__input' type="password" name='password' placeholder='Password'/>
+          <input
+            className='register__input'
+            type='password'
+            value={password}
+            onChange={handlePassword}
+            name='password'
+            placeholder='Password'/>
         </div>
         <div>        
-          <input className='register__input' type="password" name='confirmationPassword' placeholder='Confirmation Password'/>
+          <input
+            className='register__input'
+            type='password'
+            value={confirmationPassword}
+            onChange={handleConfirmationPassword}
+            name='confirmationPassword'
+            placeholder='Confirmation Password'/>
         </div>         
-        <button className='register__button' type="submit">Sign Up</button>             
+        <button
+          className='register__button'
+          type="submit">
+          Sign Up
+        </button>             
       </form> 
       <p className='register__paragraph'>Already have an account? <a href='#'>Log In</a></p>
     </section> 
