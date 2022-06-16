57da3faa79cecdff89b46ba43dc5f09d0f43f5ee
olga <oluciams84@gmail.com>
2022-06-16 15:54:56 -0500

    add route for home and about components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import { About } from './components/About';
+import { Home } from './components/Home';
 import { Login } from './components/Login';
 import { Navbar } from './components/Navbar';
 import { Signup } from './components/Signup';
@@ -11,7 +13,9 @@ function App() {
 		<Navbar/>		
 		<Routes>	
 			<Route path='/' element={<Signup/>}/>
-			<Route path='login' element={<Login/>}/>			
+			<Route path='login' element={<Login/>}/>
+			<Route path='/home' element={<Home/>}/>
+			<Route path='/about' element={<About/>}/>			
 		</Routes>
 		</>
 	);
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,12 +20,10 @@ export const Navbar =()=>{
           <svg className={active ? '' : 'none'} xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'><path d='M16.192 6.344L11.949 10.586 7.707 6.344 6.293 7.758 10.535 12 6.293 16.242 7.707 17.656 11.949 13.414 16.192 17.656 17.606 16.242 13.364 12 17.606 7.758z'/></svg>
         </button>      
         <nav className={active ? 'navbar__menu is-active' : 'navbar__menu'}>     
-          <a className='navbar__link' href="#inicio">Home</a>
-          <a className='navbar__link' href="#acerca">About</a>
+          <Link className='navbar__link' to='/home'>Home</Link>
+          <Link className='navbar__link' to="/about">About</Link>
           <Link className='navbar__link navbar__button' to='/login'>Log In</Link>      
           <Link className='navbar__link navbar__button navbar__button--dark' to='/'>Sign Up</Link>       
-          {/* <a className='navbar__link navbar__button' href='/login'>Log In</a> 
-          <a className='navbar__link navbar__button navbar__button--dark' href='/'>Sign Up</a>   */}
         </nav>
       </section>  
     </header>
