479609cf59eb1726740a9da20bc0b29362c8a4e5
olga <oluciams84@gmail.com>
2022-06-30 09:19:31 -0500

    add enviroment variable without working

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,16 +1,12 @@
 import axios from 'axios';
 
 export const signupApi = axios.create({
-  baseURL: 'http://localhost:3001',
+  // baseURL: process.env.REACT_APP_API_URL,
+  baseURL:'http://localhost:3001',
   timeout: 5000
 });
 
 export const loginApi = axios.create({
   baseURL: 'http://localhost:3001',
-  timeout: 5000,
-});
-
-export const productsApi = axios.create({
-  baseURL: 'https://fakestoreapi.com',  
   timeout: 5000,
 });
